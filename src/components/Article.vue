76add505c573904f06305c23c2fc5310019270a5
caojf <caojf@weipaitang.com>
2020-11-05 21:52:01 +0800

    文章

---
+++ src/components/Article.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="article-item">
+    <el-card class="article" :body-style="{ padding: '0px' }">
+      <div
+        v-if="article.headImageUrl"
+        class="article-image"
+        style="width: 100%; height: 200px"
+      >
+        <el-image
+          style="width: 100%; height: 100%"
+          :src="article.headImageUrl"
+          :fit="fit"
+        ></el-image>
+      </div>
+      <div class="article-content">
+        <div class="article-title">
+          <div class="article-title-context">{{ article.title }}</div>
+          <div class="articl-publised-time">{{ article.publishedAt }}</div>
+        </div>
+        <div class="article-intro">
+          {{ article.intro }}
+        </div>
+        <div class="article-bottom">
+          <el-button style="border: none;" type="danger" plain
+            >阅读全文</el-button
+          >
+          <div>
+            <div class="icon-container">
+              <i class="el-icon-view"></i>
+              <span class="articl-number">{{ article.views }}</span>
+            </div>
+            <div class="icon-container">
+              <i class="el-icon-chat-square"></i>
+              <span class="articl-number">{{ article.commentCount }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.article-item {
+  margin-bottom: 20px;
+}
+.article-content {
+  margin: 20px;
+}
+.article-title-context {
+  font-weight: 500;
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+.articl-publised-time {
+  color: #cccccc;
+  margin-bottom: 10px;
+}
+
+.article-intro {
+  margin-bottom: 10px;
+}
+
+.article-bottom {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.articl-number {
+  padding: 5px;
+}
+.el-icon-view,
+.el-icon-chat-square {
+  font-size: 24px;
+}
+.icon-container {
+  display: inline-flex;
+  align-items: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,78 +4,45 @@
       <div class="">
         <el-carousel indicator-position="outside">
           <el-carousel-item>
-            <img src="../assets/a.jpg" alt="" />
+            <img src="../assets/logo.png" alt="" />
           </el-carousel-item>
           <el-carousel-item>
-            <img src="../assets/a.jpg" alt="" />
+            <img src="../assets/logo.png" alt="" />
           </el-carousel-item>
           <el-carousel-item>
-            <img src="../assets/a.jpg" alt="" />
+            <img src="../assets/logo.png" alt="" />
           </el-carousel-item>
         </el-carousel>
       </div>
 
-      <el-row :gutter="10">
+      <el-row style="margin-top: 20px;">
         <!--left-->
-        <el-col :xs="24" :sm="14">
+        <el-col :xs="24" :sm="14" style="padding: 0">
           <div class="article-list">
-            <div class="article-item">
-              <el-card class="article" :body-style="{ padding: '0px' }">
-                <div class="article-image" style="width: 100%; height: 200px">
-                  <el-image
-                    style="width: 100%; height: 100%"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    :fit="fit"
-                  ></el-image>
-                </div>
-                <div class="article-title">
-                  <span>缭乱的漫望</span>
-                  <br />
-                  <span>四月 15, 2020</span>
-                </div>
-                <div class="article-content">
-                  时间或许过于稚嫩，脚步充斥着散漫与不经心，夏夜里适逢的一道凉风掠过发梢，就不见了匆匆一面的夏天，依稀只留了影踪在岁月的墙壁上雕刻了明与昨的年...
-                </div>
-                <div class="article-bottom">
-                  <button>开始阅读</button>
-                  <div>
-                    <i class="el-icon-view"></i>
-                    <i class="el-icon-chat-square"></i>
-                  </div>
-                </div>
-              </el-card>
-            </div>
-            <div class="article-item">
-              <el-card class="article" :body-style="{ padding: '0px' }">
-                <div class="article-image" style="width: 100%; height: 200px">
-                  <el-image
-                    style="width: 100%; height: 100%"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    :fit="fit"
-                  ></el-image>
-                </div>
-                <div class="article-title">
-                  <span>缭乱的漫望</span>
-                  <br />
-                  <span>四月 15, 2020</span>
-                </div>
-                <div class="article-content">
-                  时间或许过于稚嫩，脚步充斥着散漫与不经心，夏夜里适逢的一道凉风掠过发梢，就不见了匆匆一面的夏天，依稀只留了影踪在岁月的墙壁上雕刻了明与昨的年...
-                </div>
-                <div class="article-bottom">
-                  <button>开始阅读</button>
-                  <div>
-                    <i class="el-icon-view"></i>
-                    <i class="el-icon-chat-square"></i>
-                  </div>
-                </div>
-              </el-card>
-            </div>
+            <Article :article="article"></Article>
+            <Article :article="article"></Article>
+            <Article :article="article"></Article>
+            <Article :article="article"></Article>
+            <Article :article="article"></Article>
+            <Article :article="article"></Article>
           </div>
         </el-col>
         <!--right-->
-        <el-col :xs="24" :sm="10"
-          ><div class="grid-content bg-purple">2</div></el-col
+        <el-col :xs="24" :sm="9" :push="1"
+          ><div class="grid-content">
+            <el-card class="articl-tag-container">
+              <el-tag class="article-tag">标签一</el-tag>
+              <el-tag class="article-tag" type="success">标签二</el-tag>
+              <el-tag class="article-tag" type="info">标签三</el-tag>
+              <el-tag class="article-tag" type="warning">标签四</el-tag>
+              <el-tag class="article-tag" type="danger">标签五</el-tag>
+              <el-tag class="article-tag">标签一</el-tag>
+              <el-tag class="article-tag" type="success">标签二</el-tag>
+              <el-tag class="article-tag" type="info">标签三</el-tag>
+              <el-tag class="article-tag" type="warning">标签四</el-tag>
+              <el-tag class="article-tag" type="danger">标签五</el-tag>
+            </el-card>
+          </div></el-col
         >
       </el-row>
     </div>
@@ -83,19 +50,24 @@
 </template>
 
 <script>
+import Article from "@/components/Article";
 export default {
   name: "home",
-  components: {},
+  components: {
+    Article
+  },
   data() {
     return {
-      banners: [
-        {
-          img: "../assets/a.jpg"
-        },
-        {
-          img: "../assets/a.jpg"
-        }
-      ]
+      banners: [],
+      article: {
+        headImageUrl:
+          "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+        title: "灰度发拉架飞机圣诞快乐",
+        publishedAt: "2020-10-11 10:10",
+        intro: "福利卡江东父老卡建设路口打飞机爱丽丝发就拉世纪东方发圣诞节饭兰",
+        views: 100,
+        commentCount: 200
+      }
     };
   },
   methods: {}
@@ -119,5 +91,11 @@ export default {
 
 .bg-purple {
   background: purple;
+}
+.article-tag {
+  margin: 5px;
+}
+.articl-tag-container {
+  margin-bottom: 20px;
 }
 </style>
